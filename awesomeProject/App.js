eca0baf0141f139f8ba8e4d5d29fe905da917a76
yaboi-jepoy <qjpapenas@tip.edu.ph>
2026-03-28 17:26:24 +0800

    HoA 12.2 Supplementary

--- awesomeProject/App.js
+++ awesomeProject/App.js
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Pressable,
+} from "react-native";
 import { useState } from "react";
 
 export default function App() {
@@ -15,22 +22,31 @@ export default function App() {
       ...currentCourseGoals,
       enteredGoalText,
     ]);
+    setEnteredGoalText(""); // to clear the input after adding a goal
   }
 
   return (
     <View style={styles.appContainer}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.subtitle}>THINGS TO DO IN</Text>
+        <Text style={styles.title}>NIGHT CITY</Text>
+      </View>
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Your Course Goal"
+          placeholder="What'd it be, choom?"
           style={styles.textInput}
+          placeholderTextColor={"#d1c5c0"}
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
-        <Button title="ADD GOAL" onPress={addGoalHandler} />
+        <Pressable style={styles.addPressable} onPress={addGoalHandler}>
+          <Text style={styles.addText}>ADD</Text>
+        </Pressable>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
+        <Text style={styles.goalHeader}>Maybe I'd...</Text>
         {courseGoals.map((goal) => (
-          <Text>{goal}</Text>
+          <Text style={styles.goals}>{goal}</Text>
         ))}
       </View>
     </View>
@@ -39,28 +55,75 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    paddingTop: 50,
-    paddingHorizontal: 16,
-    paddingBottom: 24,
-    borderBottomColor: "#cccccc",
-    borderBottomWidth: 1,
     flex: 1,
+    paddingTop: 70,
+    paddingHorizontal: 16,
+    // paddingBottom: 24,
+    // justifyContent: "center",
+    backgroundColor: "#272932",
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 42,
+    fontWeight: "700",
+    marginTop: -15,
+    color: "#FDF500",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    // fontStyle: "italic",
+    color: "#37ebf3",
   },
   inputContainer: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 24,
-    alignItems: "center",
+    justifyContent: "space-around",
+    borderColor: "#1ac5b0",
+    padding: 8,
+    // marginBottom: 24,
+    // alignItems: "center",
   },
   textInput: {
-    borderWidth: 2,
-    borderColor: "#cccccc",
-    // width: "70%",
+    borderWidth: 1,
+    borderColor: "#37ebf3",
     flex: 1,
-    marginRight: 8,
+    // marginRight: 8,
     padding: 13,
   },
+  addPressable: {
+    justifyContent: "center",
+    width: "15%",
+    alignItems: "center",
+    backgroundColor: "#FDF500",
+  },
+  addText: {
+    fontWeight: "400",
+  },
   goalsContainer: {
-    flex: 5,
+    alignItems: "center",
+    borderTopColor: "#37ebf3",
+    borderTopWidth: 1,
+    borderBottomColor: "#37ebf3",
+    borderBottomWidth: 1,
+    paddingBottom: 16,
+    marginTop: 16,
+  },
+  goalHeader: {
+    margin: 8,
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 16,
+    marginBottom: 12,
+    color: "#37ebf3",
+  },
+  goals: {
+    fontSize: 16,
+    color: "#d1c5c0",
+    borderBottomColor: "grey",
+    borderBottomWidth: 1,
+    marginBottom: 4,
+    paddingHorizontal: 4,
   },
 });
